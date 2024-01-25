62cc4fdcbdea9a9c2f5e1bc0ffa90bc564941d52
jackWang <jack4shihong@gmail.com>
2024-01-26 03:19:14 +0800

    add comment for template specialization

--- 1_STL_introduction/1_9_1_config3.cpp
+++ 1_STL_introduction/1_9_1_config3.cpp
@@ -11,13 +11,31 @@
 #include <iostream>
 using namespace std;
 
+// C++中的模板编译期间会实例化, 使用这个工具可以查看实例化的结果: https://cppinsights.io/
 template<typename T>
 class TestClass {
 public:
+	// 这里的static声明int是一个静态变量, 未来所有实例化的对象都会共享同一个变量
 	static int _data;
 };
 
-// 需要加上template<>，否则编译出错，进行内存配置
+/**
+ * template<>
+ * class TestClass<int> {
+ *   public: 
+ *   = 所有的TestClass<int>类的实例共享同一个遍历
+ *   static int _data;
+ * };
+ * 
+ * template<>
+ * class TestClass<char> {
+ *   public: 
+ *   = 所有的TestClass<char>类的实例共享同一个遍历
+ *   static int _data;
+ * };
+*/
+
+// C++模板特例化, 这两行代码会首先实例化出上面的模板实例, 然后再赋值
 template<>
 int TestClass<int>::_data = 1;
 template<>
@@ -33,7 +51,9 @@ int main() {
 	cout << obji1._data << " " << obji2._data << endl;
 	cout << objc1._data << " " << objc2._data << endl;
 
+	// obji1和obji2共享同一个data, 所以任意一个修改, 其他的都会改变
 	obji1._data = 3;
+	// 同理, objc2修改data, objc1的值也会改变
 	objc2._data = 4;
 
 	cout << obji1._data << " " << obji2._data << endl;
